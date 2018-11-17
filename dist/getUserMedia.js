bb0ba31a6dc8dd9c03bb9922257c572091f4452e
Benjamin Kluck <ben@fug.lu>
2018-11-17 14:29:48 +0100

    Add dist folder

---
+++ dist/getUserMedia.js
@@ -0,0 +1,5 @@
+export default navigator.getUserMedia ? navigator.getUserMedia : navigator.mediaDevices ?
+    navigator.mediaDevices.getUserMedia :
+    (navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia);
